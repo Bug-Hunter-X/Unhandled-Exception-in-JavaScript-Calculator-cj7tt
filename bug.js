b8b3bf38a123d6baa4bde7d5717c06b020bcff81
BugHunter-X <askzenai@gmail.com>
2025-01-25 23:06:11 +0100

    Add bug.js file

---
+++ bug.js
@@ -0,0 +1,24 @@
+function add(a, b) {
+  return a + b;
+}
+
+function subtract(a, b) {
+  return a - b;
+}
+
+function multiply(a, b) {
+  return a * b;
+}
+
+function divide(a, b) {
+  if (b === 0) {
+    throw new Error('Cannot divide by zero');
+  }
+  return a / b;
+}
+
+console.log(add(5, 3)); // Output: 8
+console.log(subtract(10, 4)); // Output: 6
+console.log(multiply(7, 2)); // Output: 14
+console.log(divide(9, 3)); // Output: 3
+console.log(divide(9,0)); // throws an error
